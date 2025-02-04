f5402b0c59b533f403c5bc04bea42df33efa80dc
hameast20 <ejsflqm17@gmail.com>
2025-02-04 15:09:43 +0900

    Modify Server & GUI

--- control_gui/control_gui.py
+++ control_gui/control_gui.py
@@ -1,20 +1,182 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import QThread, QTimer
 from PyQt5 import uic
 import cv2
 
-from_class = uic.loadUiType("./Control_GUI/Control_GUI.ui")[0]
+import socket
+import select
+import threading
+import numpy as np
 
-class WindowClass(QDialog, from_class):
+import time
+
+# Main Server
+MAIN_SERVER = 0
+MAIN_ADDR = "192.168.0.8"  # 서버의 IP 주소 또는 도메인 이름
+MAIN_PORT = 8081       # 포트 번호
+
+AI_IP = "192.168.0.8"
+UDP_PORT1 = 9506
+
+udp_listnener_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_listnener_sock.bind((AI_IP, UDP_PORT1))
+imgListen_thread_flag = True
+
+def IMGListener(window, udp_socket0, set_time):
+    # udp_socket0 = Listnener
+    global imgListen_thread_flag
+
+    socketList = [udp_socket0]
+    while imgListen_thread_flag:
+
+        read_socket, _, _ = select.select(socketList, [], [], 1)
+        for sock in read_socket:
+            data, _ = sock.recvfrom(65535)
+
+            pyri_id = data[0]
+            img_data = data[1:]
+            encoded_img = np.frombuffer(img_data, dtype = np.uint8)
+            frame = cv2.imdecode(encoded_img,  cv2.IMREAD_COLOR_BGR)
+
+            h,w,c = frame.shape
+            qimage = QImage(frame.data, w,h,w*c, QImage.Format_RGB888)
+
+            window.pixmap = window.pixmap.fromImage(qimage)
+
+            if pyri_id == 1:
+                lbl = window.lbl_cam1
+            elif pyri_id == 2:
+                lbl = window.lbl_cam2
+
+            window.pixmap = window.pixmap.scaled(lbl.width(), lbl.height())
+            lbl.setPixmap(window.pixmap)
+
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     pass
+
+def requestTCP(messages):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((MAIN_ADDR, MAIN_PORT))
+
+    client_socket.send(f"{'&&'.join(messages)}".encode('utf-8'))
+
+    response = "Error" # Default
+    ready = select.select([client_socket], [], [], 2)
+    if ready[0]:
+        response = client_socket.recv(2048).decode('utf-8')
+    
+    client_socket.close()
+    return response
+
+def draw_transparent_circle(img, center_xy):
+    """
+        img 이미지 파일
+        center_xy: 원의 중심 좌표, tuple
+    """
+
+    radius = 30
+    # 초록색 (Green) with Alpha (투명도)
+    color = (0, 255, 0, 128)  # 마지막 128은 Alpha 값으로, 0.5 =  255 / 2 ~= 128
+    alpha = 0.5
+
+    try:
+        # RGBA 이미지를 생성합니다.
+        if img.shape[2] == 3:  # 만약 이미지가 RGB 이미지라면
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)  # BGR에서 BGRA로 변경합니다.
+        overlay = img.copy()
+        cv2.circle(overlay, center_xy, radius, color, -1)
+        # Opacity 적용
+        cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0, img)
+        return img
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+    
+#######################################GUI PART########################################################
+main_ui = uic.loadUiType("/home/hdk/ws/final_project/data/test_main.ui")[0]
+
+class WindowClass(QDialog, main_ui):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        # self.synctimer = SyncTimer(self)
+        # self.synctimer.daemon = True
+        # self.synctimer.update.connect(self.updateSyncData)
+        self.timer_1sec = QTimer()
+        self.timer_1sec.setInterval(500)
+        self.timer_1sec.timeout.connect(self.updateSyncData)
+        self.timer_1sec.start()
 
         self.pixmap = QPixmap()
+
+        self.img = cv2.imread("/home/hdk/ws/final_project/data/pyri_map_final.png")
+        self.img = cv2.rotate(self.img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+        cvt_img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+        h,w,c = cvt_img.shape
+        qimage = QImage(cvt_img.data, w,h,w*c, QImage.Format_RGB888)
+
+        self.pixmap = self.pixmap.fromImage(qimage)
+        self.pixmap = self.pixmap.scaled(self.lbl_map.width(), self.lbl_map.height())
+        self.lbl_map.setPixmap(self.pixmap)
+        self.btn_test1.clicked.connect(self.btn_test1Clicked)
+
+    def updateSyncData(self):
+        # 시간 업데이트
+        now = time.localtime()
+        self.lbl_time.setText(time.strftime("%Y-%m-%d %H:%M:%S", now))
+        # 서버로부터 정보 동기화
+        messages = ["SyncData"]
+        response = requestTCP(messages)
+        parts = response.split("&&")
+
+        if parts[0] == "SyncData":
+            if parts[1] == "ideal":
+                pose_x = float(parts[2])
+                pose_y = float(parts[3])
+                odom_x = float(parts[4])
+                odom_y = float(parts[5])
+
+                test_text = f"""pose_x: {pose_x}\npose_y: {pose_y}\nodom_x: {odom_x}\nodom_y: {odom_y}"""
+                self.lbl_test1.setText(test_text)
+            elif parts[1] == "aiResult":
+                self.lbl_test2.setText(parts[2])
+
+        else:
+            print("Wrong Response: ", parts)
+
+    def btn_test1Clicked(self):
+        # self.img = cv2.imread("/home/hdk/ws/final_project/data/pyri_map_final.png")
+        # self.img = cv2.rotate(self.img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+        center_xy = (int(self.le_test1.text()), int(self.le_test2.text()))
+
+        self.img = draw_transparent_circle(self.img, center_xy)
+
+        cvt_img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+        h,w,c = cvt_img.shape
+        qimage = QImage(cvt_img.data, w,h,w*c, QImage.Format_RGB888)
+
+        self.pixmap = self.pixmap.fromImage(qimage)
+        self.pixmap = self.pixmap.scaled(self.lbl_map.width(), self.lbl_map.height())
+        self.lbl_map.setPixmap(self.pixmap)
+
+
+######################################################################################################
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWindows = WindowClass()
+    imgListenTread = threading.Thread(target=IMGListener, 
+                                        args=(myWindows, udp_listnener_sock, 300))
     myWindows.show()
+
+    imgListenTread.start()
+
+
+    print("hello")
+    # imgListenTread.join()
     sys.exit(app.exec_())
--- tokkih_control_server/src/tokkih_service.py
+++ tokkih_control_server/src/tokkih_service.py
@@ -3,6 +3,7 @@
 import threading
 import socket
 import select
+import json
 
 # ROS용 import
 import rclpy as rp
@@ -11,26 +12,75 @@ from turtlesim.msg import Color
 from std_msgs.msg import Float32
 from sensor_msgs.msg import MagneticField
 
+from tf2_msgs.msg import TFMessage
+from geometry_msgs.msg import PoseStamped
+
 # 기타 import
 import time
 import signal
 import sys
+import numpy as np
+import cv2
 
-# SolCareGUI 수신 소켓 설정
-host0 = "192.168.0.48"  # 서버의 IP 주소 또는 도메인 이름
-port0 = 8081       # 포트 번호
+MY_IP = "192.168.0.8"
+
+# from Tokkih GUI 수신 소켓 설정
+TCP_PORT0 = 8081       # 포트 번호
 server_socket0 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket0.bind((host0, port0))
+server_socket0.bind((MY_IP, TCP_PORT0))
 server_socket0.listen()
 userListen_thread_flag = True
+
+# Pyri로 부터의 영상 udp 수신 소켓 설정
+UDP_PORT0 = 9505
+udp_listnener_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_listnener_sock.bind((MY_IP, UDP_PORT0))
+imgListen_thread_flag = True
+
+# 영상을 다시 AI서버로 보내줄 소켓 seoljeong
+AI_IP = "192.168.0.9"
+UDP_PORT1 = 9506
+udp_talker_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# 영상을 다시 GUI로 보내줄 소켓 seoljeong
+GUI_IP = "192.168.0.8"
+UDP_PORT2 = 9506
 
 # 전역 변수
 pose_data = Pose()
 color_data = Color()
+tp_data = [0.0, 1.1, 2.2]
+tf_data = [1.1, 2.2, 3.3, 4.4]
+
+ai_result_flag = False
+received_json = dict()
+ai_result_name = "aiResult"
+
+
+temp_img = np.zeros((240,320,3), dtype=np.int8)
+_, buffer = cv2.imencode('.jpg', temp_img)
+temp_img_b = b'\x02' + buffer.tobytes()
+
+def IMGListener(udp_socket0, udp_socket1, set_time):
+    # udp_socket0 = Listnener
+    # udp_socket1 = Talker to AI Server
+    # udp_socket1 = Talker to GUI
+    global imgListen_thread_flag
+
+    data = temp_img_b
+    socketList = [udp_socket0]
+
+    while imgListen_thread_flag:
+        read_socket, _, _ = select.select(socketList, [], [], 1)
+        for sock in read_socket:
+            data, _ = sock.recvfrom(65535)
+            udp_socket1.sendto(data, (AI_IP, UDP_PORT1))
+            udp_socket1.sendto(data, (GUI_IP, UDP_PORT2))
+
 
 
 def TCPListener(server_socket, set_time):
-    global userListen_thread_flag, pose_data, color_data
+    global userListen_thread_flag, pose_data, color_data, ai_result_flag
     print("hello thread")
 
     cnt = 0
@@ -43,52 +93,73 @@ def TCPListener(server_socket, set_time):
             if cnt >= set_time:
                 print("end thread by cnt")
                 userListen_thread_flag = False
+
+                signal_handler(None, None)
                 break
 
-        print("Wait accept")
+        # print("Wait accept")
         read_socket, write_socket, error_socket = select.select(socketList, [], [], 1)
         for sock in read_socket :
             if not userListen_thread_flag:
                 break
             elif sock == server_socket :
-                print("나 아닌데")
                 client_socket, client_address = sock.accept()
                 socketList.append(client_socket)
             else :
                 try:
-                    print("Wait recive")
+                    # print("Wait recive")
                     # 클라이언트로부터 요청 받기
                     data = sock.recv(1024).decode("utf-8")
                     if not data:
                         print("No Data")
                         continue
 
-                    # 요청 파싱
-                    parts = data.split("&&")
-                    print(parts[:3])
-                    if len(parts) != 0:
-                        if parts[0] == "test_client":
-                            print("Hello~")
+                    # data='[{"bbox": [138.38656616210938, 116.25907897949219, 221.21823120117188, 205.5818634033203], "confidence": 0.522793173789978, "class_id": 56}, {"bbox": [0.0, 176.331298828125, 41.553306579589844, 206.30224609375], "confidence": 0.5043512582778931, "class_id": 45}, {"bbox": [96.24269104003906, 75.76333618164062, 127.29405212402344, 131.20449829101562], "confidence": 0.26554909348487854, "class_id": 41}]'
 
-                            messages = ["test_client"]
-                            messages.append("Hello client, this is ROS server")
-                            sock.send("&&".join(messages).encode('utf-8'))
+                    try:
+                        received_json = json.loads(data)[0]
+                        ai_result_flag = True
+                    except:
+                        # 요청 파싱
+                        parts = data.split("&&")
+                        # print(parts[:3])
+                        if len(parts) != 0:
+                            if parts[0] == "test_client":
+                                print("Hello~")
 
-                        elif parts[0] == "RequestPose":
-                            print("Hello~")
+                                messages = ["test_client"]
+                                messages.append("Hello client, this is ROS server")
+                                sock.send("&&".join(messages).encode('utf-8'))
 
-                            messages = ["RequestPose"]
-                            messages.append(str(pose_data))
-                            sock.send("&&".join(messages).encode('utf-8'))
-                        elif parts[0] == "RequestColor":
-                            print("Hello~")
+                            elif parts[0] == "RequestPose":
 
-                            messages = ["RequestColor"]
-                            messages.append(str(color_data))
-                            sock.send("&&".join(messages).encode('utf-8'))
-                        else:
-                            print("뭐 이상한 거 왔어요")
-                            print(parts)
+                                messages = ["RequestPose"]
+                                messages.append(str(pose_data))
+                                sock.send("&&".join(messages).encode('utf-8'))
+                            elif parts[0] == "RequestColor":
+
+                                messages = ["RequestColor"]
+                                messages.append(str(color_data))
+                                sock.send("&&".join(messages).encode('utf-8'))
+                            elif parts[0] == "SyncData":
+
+                                messages = ["SyncData"]
+
+                                if ai_result_flag:
+                                    messages.append(ai_result_name)
+                                    messages.append(str(received_json))
+
+                                    ai_result_flag = False
+                                else:
+                                    messages.append("ideal")
+                                    for val in tf_data:
+                                        messages.append(str(val))
+
+                                sock.send("&&".join(messages).encode('utf-8'))
+
+                            else:
+                                print("뭐 이상한 거 왔어요")
+                                print(parts)
                 except Exception as e:
                     print(f"오류 발생: {e}")
 
@@ -104,10 +175,6 @@ def TCPListener(server_socket, set_time):
 
 ### 아직 어떻게 할 지 생각 중
 ########################################################################################################
-class DataStructure:
-    def __init__(self):
-        self.pose_data = Pose()
-        self.color_data = Color()
 
 
 def pose_callback(data):
@@ -118,32 +185,101 @@ def color_callback(data):
     global color_data
     color_data = data
 
+def tf_callback(data):
+    global tf_data
+    # tf_data = data
+
+    map_frame = data.transforms[0]
+    odom_frame = data.transforms[1]
+
+    tf_data = [float(map_frame.transform.translation.x), 
+                float(map_frame.transform.translation.y),
+                float(odom_frame.transform.translation.x),
+                float(odom_frame.transform.translation.x)]
+    # print(map_frame.header.frame_id, "//", map_frame.child_frame_id)
+    # print("time ==> ", map_frame.header.stamp)
+    # print("pose ==> ", map_frame.transform.translation.x, map_frame.transform.translation.y)
+    # print("-----------")
+    # print(odom_frame.header.frame_id, "//", odom_frame.child_frame_id)
+    # print("time ==> ", odom_frame.header.stamp)
+    # print("pose ==> ", odom_frame.transform.translation.x, odom_frame.transform.translation.y)
+
+def tp_callback(data):
+    # print(data)
+
+    global tp_data
+    pose = data.pose.position
+
+    tp_data = [float(pose.x),
+                float(pose.y),
+                float(pose.z)]
+
+    print(tp_data)
+
+
 rp.init()
 server_node =  rp.create_node('server_node')
-server_node.create_subscription(Pose, 'turtle1/pose', pose_callback, 10)
-server_node.create_subscription(Color, 'turtle1/color_sensor', color_callback, 10)
+server_node.create_subscription(TFMessage, '/tf', tf_callback, 10)
+server_node.create_subscription(PoseStamped, '/tracked_pose', tp_callback, 10)
+rosListen_thread_flag = True
+
+server_node
+
+def ROSListener(s_node):
+    global rosListen_thread_flag
+
+    while rosListen_thread_flag:
+        print("test1")
+        rp.spin_once(s_node)
+        print("test2")
+
+    s_node.destroy_node()
+    print("rosListner close")
+
 
 ########################################################################################################
 
 
 def signal_handler(signal, frame):
-    global userListen_thread_flag
+    global userListen_thread_flag, imgListen_thread_flag, rosListen_thread_flag
 
-    print('You pressed Ctrl+C!', signal)
+    print('\nsignal_handler called', signal)
     userListen_thread_flag = False
+    imgListen_thread_flag = False
+    rosListen_thread_flag = False
+    
+    cv2.destroyAllWindows()
+    time.sleep(1)
+
+    for th in threading.enumerate():
+        print(th.name)
 
     server_socket0.close()
+    rp.shutdown()
     # remote.close()
-    server_node.destroy_node()
 
-    time.sleep(1)
     print("Good Bye")
+    time.sleep(1)
     sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
 
 
 if __name__=="__main__":
-    signal.signal(signal.SIGINT, signal_handler)
-    userListenTread = threading.Thread(target=TCPListener, args=(server_socket0, -1))
+    print("signal setting")
+    userListenTread = threading.Thread(target=TCPListener, 
+                                        args=(server_socket0, -1))
+    imgListenTread = threading.Thread(target=IMGListener, 
+                                        args=(udp_listnener_sock, udp_talker_sock, -1))
+    rosListenTread = threading.Thread(target=ROSListener, 
+                                        args=(server_node,))
     
     userListenTread.start()
-    rp.spin(server_node)
+    imgListenTread.start()
+    rosListenTread.start()
+
+    userListenTread.join()
+    imgListenTread.join()
+    rosListenTread.join()
+
+
